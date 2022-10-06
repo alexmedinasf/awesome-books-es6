1ba1876db9d55924c682b2dc0c098ccb8b6f2294
alexmedinasf <alexsales001@hotmail.com>
2022-10-06 11:04:47 -0300

    linters

--- modules/books.js
+++ modules/books.js
@@ -3,11 +3,13 @@ export default class Book {
     this.title = title;
     this.author = author;
   }
+  
     addBook = () => {
       const books = JSON.parse(localStorage.getItem('books')) || [];
       books.push(this);
       localStorage.setItem('books', JSON.stringify(books));
     };
+
     removeBook = () => {
       const books = JSON.parse(localStorage.getItem('books')) || [];
       const filteredBooks = books.filter((book) => book.title !== this.title);
